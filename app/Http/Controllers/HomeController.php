89f7b1851a3b60ef73df84b899ce62b0a7c6e68c
Dais Nurfaizi <dsnurfaizi@gmail.com>
2020-10-13 22:58:40 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\ChatSent;
+use App\Models\Chat;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function sendMessage(Request $request)
+    {
+        $message = $request->user()->chats()->create([
+            'message' => $request->message
+        ]);
+        broadcast(new ChatSent($message->load('user')))->toOthers();
+        return ['status' => 'success'];
+    }
+
+    public function fetchMessage()
+    {
+        return Chat::with('user')->get();
+    }
+}
---
+++ resources/js/components/chat/RoomComponent.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Chat Room</div>
+
+          <div id="chatbox" class="card-body bg-secondary">
+            <div
+              class="media m-2 bg-light rounded"
+              v-for="(chat, index) in messages"
+              :key="index"
+            >
+              <div class="media-body m-2">
+                <h5 class="mt-0">{{ chat.user.name }}</h5>
+                {{ chat.message }}
+              </div>
+            </div>
+          </div>
+          <div class="card-footer">
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="message"
+                @keyup.enter="sendMessage()"
+                @keydown="typingEvent()"
+              />
+            </div>
+            <p class="text-muted" v-if="userTyping">
+              {{ userTyping.name }} is typing...
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-header">Users Online</div>
+          <div id="user-online" class="card-body bg-secondary">
+            <ul class="list-group">
+              <li
+                class="list-group-item mt-3"
+                v-for="(user, index) in users"
+                :key="index"
+              >
+                <img
+                  src="https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-mr-robot-rami-malek.jpg"
+                  class="rounded-circle"
+                  alt="Cinque Terre"
+                  width="50"
+                  height="50"
+                  style="display: inline-block"
+                />
+                <blockquote class="blockquote" style="display: inline-block">
+                  {{ user.name }}
+                </blockquote>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+    <script>
+export default {
+  data() {
+    return {
+      message: "",
+      messages: [],
+      users: [],
+      userTyping: false,
+      typingTimer: false,
+    };
+  },
+  props: {
+    user: Object,
+  },
+  methods: {
+    fetchMessage() {
+      axios
+        .get("/fetch")
+        .then((result) => {
+          this.messages = result.data;
+          console.log(result.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    typingEvent() {
+      Echo.join("chat").whisper("typing", this.user);
+    },
+    sendMessage() {
+      this.messages.push({
+        user: this.user,
+        message: this.message,
+      });
+      axios
+        .post("/send", {
+          message: this.message,
+        })
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.message = "";
+      //   console.log(this.message);
+    },
+    scrollDown() {
+      let container = document.getElementById("chatbox");
+      let scrollHeight = container.scrollHeight;
+      container.scrollTop = scrollHeight;
+    },
+  },
+  mounted() {
+    this.fetchMessage();
+
+    Echo.join("chat")
+      .here((users) => {
+        this.users = users;
+      })
+      .joining((user) => {
+        this.users.push(user);
+        console.log(user.name);
+      })
+      .leaving((user) => {
+        this.users = this.users.filter((u) => u.id != user.id);
+        console.log(user.name);
+      })
+      .listen("ChatSent", (e) => {
+        this.messages.push(e.message);
+        console.log(e);
+      })
+      .listenForWhisper("typing", (user) => {
+        this.userTyping = user;
+        if (this.typingTimer) {
+          clearTimeout(this.typingTimer);
+        }
+        this.typingTimer = setTimeout(() => {
+          this.userTyping = false;
+        }, 1500);
+      });
+    this.scrollDown();
+  },
+  updated() {
+    this.scrollDown();
+  },
+};
+</script>
+<style lang="css" scoped>
+#chatbox {
+  overflow: auto;
+  height: 300px;
+}
+</style>
